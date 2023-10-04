de3fa41de289e62679c13310f8abe93493a5f26f
jpcarrizo <juampics@gmail.com>
2023-10-04 16:42:58 -0300

    modifico el urls.py del proyecto

--- Arte/Arte/urls.py
+++ Arte/Arte/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('GaleriaApp/', include('GaleriaApp.urls')),
+    path('', include('GaleriaApp.urls')),
     
 ]
